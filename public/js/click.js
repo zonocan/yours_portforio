99fbf8dd2b3bb490c15a4d0d2e819034c1ec0784
zonocan <zonocan.y.m@icloud.com>
2022-05-19 20:13:51 +0900

    indexのボタン追加完了

--- public/js/click.js
+++ public/js/click.js
@@ -7,17 +7,41 @@ $(function() {
 
   $('#profile').on('click', function() {
 
-    $('.introduction').toggle('slow', alertFunc);
+    $('.profile_introduction').toggle('slow', alertFunc);
 
-    $('.line').toggle('slow', alertFunc);
+    $('.profile_line').toggle('slow', alertFunc);
 
   });
 
-//プロフィールボタンのテキストの変更
+
+//強みの表示
+
+  $('#strong_point').on('click', function() {
+
+    $('.strong_introduction').toggle('slow', alertFunc);
+
+    $('.strong_line').toggle('slow', alertFunc);
+
+  });
+
+
+//スキルの表示
+
+  $('#skil').on('click', function() {
+
+    $('.skil_introduction').toggle('slow', alertFunc);
+
+    $('.skil_line').toggle('slow', alertFunc);
+
+  });
+
 
   function alertFunc() {
 
-    if ($(this).css('display') == 'none') {
+
+//プロフィールボタンのテキストの変更
+
+    if ($('.profile_introduction').css('display') == 'none') {
 
       $("#profile").text("PROFILE ▶︎");
 
@@ -26,7 +50,34 @@ $(function() {
       $("#profile").text("CLOSE ×");
 
     }
+
+//強みのテキストの変更
+
+    if ($('.strong_introduction').css('display') == 'none') {
+
+      $("#strong_point").text("STRONG ▶︎");
+
+    } else {
+
+      $("#strong_point").text("CLOSE ×");
+
+    }
+
+
+    //プロフィールボタンのテキストの変更
+
+    if ($('.skil_introduction').css('display') == 'none') {
+
+      $("#skil").text("SKILS ▶︎");
+
+    } else {
+
+      $("#skil").text("CLOSE ×");
+
+    }
+
   };
 
-  
+
+
 });
